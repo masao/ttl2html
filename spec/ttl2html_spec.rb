d748899d3d1447060618c9aa1a4a2ad3ff58b5c9
Masao Takaku <tmasao@acm.org>
2022-01-03 11:48:19 +0900

    fix link to home and about at navbar. fix #91

--- spec/ttl2html_spec.rb
+++ spec/ttl2html_spec.rb
@@ -304,6 +304,19 @@ RSpec.describe TTL2HTML::App do
       expect(html).to have_css("footer p", text: "Dataset Admin")
       expect(html).to have_css("footer p", text: "© 2021")
     end
+    it "should link to home and about" do
+      ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example.yml"))
+      ttl2html.load_turtle(File.join(spec_base_dir, "example/example.ttl"))
+      ttl2html.output_html_files
+      cont = open("/tmp/html/index.html"){|io| io.read }
+      html = Capybara.string cont
+      expect(html).to have_css("nav.navbar a.nav-link[href='./']", text: "Home")
+      expect(html).to have_css("nav.navbar a.nav-link[href='about.html']", text: "About")
+      cont = open("/tmp/html/a/b.html"){|io| io.read }
+      html = Capybara.string cont
+      expect(html).to have_css("nav.navbar a.nav-link[href='../']", text: "Home")
+      expect(html).to have_css("nav.navbar a.nav-link[href='../about.html']", text: "About")
+    end
     it "should output logo" do
       ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example_logo.yml"))
       ttl2html.load_turtle(File.join(spec_base_dir, "example/example.ttl"))
