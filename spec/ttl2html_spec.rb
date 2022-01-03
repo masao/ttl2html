3f4e618897c90e44131f6666f04e8104ae3a4a26
Masao Takaku <tmasao@acm.org>
2022-01-03 13:38:02 +0900

    add additional_link setting. fix #92

--- spec/ttl2html_spec.rb
+++ spec/ttl2html_spec.rb
@@ -330,6 +330,15 @@ RSpec.describe TTL2HTML::App do
       expect(html).to have_css("nav.navbar > a.navbar-brand img[src='../logo.png']")
       expect(html).to have_css("nav.navbar > a.navbar-brand[href='../']")
     end
+    it "should output additional links" do
+      ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example_additional_link.yml"))
+      ttl2html.load_turtle(File.join(spec_base_dir, "example/example.ttl"))
+      ttl2html.output_html_files
+      cont = open("/tmp/html/index.html"){|io| io.read }
+      html = Capybara.string cont
+      expect(html).to have_css("nav.navbar a.nav-link[href='https://example.com/']", text: "Link1")
+      expect(html).to have_css("nav.navbar a.nav-link[href='https://example.org/']", text: "Link2")
+    end
     it "should output breadcrumbs according to the settings" do
       ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example_breadcrumbs.yml"))
       ttl2html.load_turtle(File.join(spec_base_dir, "example/example_breadcrumbs.ttl"))
