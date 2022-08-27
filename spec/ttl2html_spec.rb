afc04c95182014f0aa868a6a50b2e2898b8fe076
Masao Takaku <tmasao@acm.org>
2022-08-27 16:47:22 +0900

    add id attr for each shape. fix #67

--- spec/ttl2html_spec.rb
+++ spec/ttl2html_spec.rb
@@ -258,6 +258,16 @@ RSpec.describe TTL2HTML::App do
       expect(html).to have_css("p", text: "This class represents a Book instance.")
       expect(html).to have_title(/^About Test website$/)
     end
+    it "should generate id attr for shape headings" do
+      ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example.yml"))
+      ttl2html.load_turtle(File.join(spec_base_dir, "example/shape.ttl"))
+      ttl2html.output_html_files
+      cont = open("/tmp/html/about.html"){|io| io.read }
+      html = Capybara.string cont
+      puts cont
+      expect(html).to have_css("h2#shapes")
+      expect(html).to have_css("h3#AShape", text: "Book")
+    end
     it "should generate resouce instance with the shape order" do
       ttl2html = TTL2HTML::App.new(File.join(spec_base_dir, "example/example.yml"))
       ttl2html.load_turtle(File.join(spec_base_dir, "example/shape_order.ttl"))
