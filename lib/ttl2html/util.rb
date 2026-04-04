f39a75a4df95825b448164854cace9c0570a4c63
Masao Takaku <tmasao@acm.org>
2026-04-04 20:33:30 +0900

    use set cache for speedup uri_mapping_to_path(). #228

--- lib/ttl2html/util.rb
+++ lib/ttl2html/util.rb
@@ -1,11 +1,20 @@
 module TTL2HTML
   module Util
+    require "set"
+
     def make_mapping_uris_cache(param)
-      @path_cache = []
+      @path_cache = Set.new
       data = @data || @param[:data_global] || @param[:data] || {}
+
       data.keys.each do |uri|
-        local_path =_uri_mapping_to_path(uri, param)
-        @path_cache << local_path
+        local_path = _uri_mapping_to_path(uri, param)
+        # 親パスをすべて抽出してSetに追加
+        idx = 0
+        while (idx = local_path.index("/", idx))
+          parent = local_path[0...idx]
+          @path_cache << parent unless parent.empty?
+          idx += 1
+        end
       end
       @path_cache
     end
@@ -24,10 +33,10 @@ module TTL2HTML
     end
     def uri_mapping_to_path(uri, param, suffix = ".html")
       path = nil
-      @path_cache = make_mapping_uris_cache(param) if @path_cache.nil?
+      @path_cache ||= make_mapping_uris_cache(param)
       path = _uri_mapping_to_path(uri, param, suffix)
       if suffix == ".html"
-        if @path_cache.find{|e| e.start_with?(path + "/") }
+        if @path_cache.include? path
           path += "/index"
         elsif path.end_with?("/")
           path += "index"
